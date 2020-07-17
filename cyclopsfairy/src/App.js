43229552201e01b27b15e042676a93edeb976d74
miss jody <missjodyrussell@gmail.com>
2020-07-16 19:12:52 -0600

    finally fixed sun animation

--- cyclopsfairy/src/App.js
+++ cyclopsfairy/src/App.js
@@ -6,68 +6,86 @@ import Particles from 'react-particles-js';
 function App() {
   return (
 	  <div className="App">
-		  <div id="homepage-text">
-			  <h1>
-					  »my name is miss jody«
-			  </h1>
 
-			<h3>
-				  i am a full-stack web developer in love with front end design.
-			</h3>
-			  <h3>
-				  i am a classically trained watercolorist who prefers the digital work process.
-			  </h3>
-			  <h4>  なぜ白人は美学のために他の文化を使うのですか </h4>
-			  <h4> choose action» </h4>
-			  <h4> »» programming projects </h4>
-			  <h4> »» artwork </h4>
-			  <h4> »» free stuff </h4>
-			  <h4> »» contact </h4>
-			  </div>
-		 <Particles
-		 params={{
-			"particles": {
-				"number": {
-					"value": 60,
-					"density": {
-						"enable": true,
-						"value_area": 1500
-					}
-				},
-				"line_linked": {
-					"enable": true,
-					"opacity": 0.05
-				},
-				"move": {
-					"direction": "right",
-					"speed": 0.3
-				},
-				"size": {
-					"value": 1
-				},
-				"opacity": {
-					"anim": {
-						"enable": true,
-						"speed": 1,
-						"opacity_min": 0.05
-					}
-				}
-			},
-			"interactivity": {
-				"events": {
-					"onclick": {
-						"enable": true,
-						"mode": "repulse"
-					}
-				},
-				"modes": {
-					"push": {
-						"particles_nb": 1
-					}
-				}
-			},
-			"retina_detect": true
+
+		  <Particles
+			  params={{
+				  "color": "#D2691E",
+				  "shape": {
+					  "type": "circle",
+					  "stroke": {
+						  "width": 1,
+						  "color": "#D2691E"
+					  }
+                  },
+				  "fps_limit": 28,
+				  "particles": {
+					  "number": {
+						  "value": 200,
+						  "density": {
+							  "enable": false
+						  }
+					  },
+					  "line_linked": {
+						  "enable": true,
+						  "distance": 30,
+						  "opacity": 0.4
+					  },
+					  "move": {
+						  "speed": 1
+					  },
+					  "opacity": {
+						  "anim": {
+							  "enable": true,
+							  "opacity_min": 0.05,
+							  "speed": 2,
+							  "sync": false
+						  },
+						  "value": 0.7
+					  }
+				  },
+				  "line_linked": {
+					  "color": "#D2691E",
+					  "width": 2,
+					  "enable": true,
+					  "distance": 200,
+					  "opacity": 0.5
+                  },
+				  "polygon": {
+					  "enable": true,
+					  "scale": 0.5,
+					  "type": "inline",
+					  "move": {
+						  "radius": 10
+					  },
+					  "url": "/suntest.svg",
+					  "inline": {
+						  "arrangement": "equidistant"
+					  },
+					  "draw": {
+						  "enable": true,
+						  "stroke": {
+							  "color": "rgba(210,105,30, .2)"
+						  }
+					  }
+				  },
+				  "retina_detect": false,
+				  "interactivity": {
+					  "events": {
+						  "onhover": {
+							  "enable": true,
+							  "mode": "bubble"
+						  }
+					  },
+					  "modes": {
+						  "bubble": {
+							  "size": 6,
+							  "distance": 40
+						  }
+					  }
+				  }
 			  }} />
+		
 
 		 
 
